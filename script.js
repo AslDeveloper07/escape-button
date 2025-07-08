e554ab054cf965b6fcc7c89b38ca392238cc6501
AslDeveloper07 <aslwexking777@gmail.com>
2025-07-08 11:55:47 +0500

    addd

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const $$ = es => document.querySelectorAll(es);
 
 var btn = $('button');
 var wrap = $('.wrap');
-btn.addEventListener('click', ()=>{
-    alert('Winner winner chicken dinner🎉🎊🎁');
-})
-btn.addEventListener('mousemove', ()=>{
+
+btn.addEventListener('click', () => {
+    alert('Winner winner chicken dinner 🎉🎊🎁');
+});
+
+btn.addEventListener('mousemove', () => {
     btn.style.top = Math.floor(Math.random() * (wrap.clientHeight - btn.clientHeight)) + "px";
     btn.style.left = Math.floor(Math.random() * (wrap.clientWidth - btn.clientWidth)) + "px";
-})
+});
